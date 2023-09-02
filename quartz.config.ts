ec4b95d368d3aea501c3ae0668031514b109d9a6
Guillaume Ansanay-Alex <gansanay@gmail.com>
2023-09-02 18:31:16 +0200

    Quartz sync: Sep 2, 2023, 6:31 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,19 +3,20 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Guillaume Ansanay-Alex - Notes sur la transformation data",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "gansanay.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Libre Franklin",
+        body: "Lora",
+        // body: "Libre Baskerville",
         code: "IBM Plex Mono",
       },
       colors: {
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -13,27 +13,53 @@ export const sharedPageComponents: SharedLayout = {
   }),
 }
 
+// FOR MY INFORMATION
+// export interface FullPageLayout {
+//   head: QuartzComponent // single component
+//   header: QuartzComponent[] // laid out horizontally
+//   beforeBody: QuartzComponent[] // laid out vertically
+//   pageBody: QuartzComponent // single component
+//   left: QuartzComponent[] // vertical on desktop, horizontal on mobile
+//   right: QuartzComponent[] // vertical on desktop, horizontal on mobile
+//   footer: QuartzComponent // single component
+// }
+
+
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
+  header: [Component.PageTitle(), Component.Search(), Component.Darkmode()],
   beforeBody: [Component.ArticleTitle(), Component.ContentMeta(), Component.TagList()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
-    Component.DesktopOnly(Component.TableOfContents()),
-  ],
-  right: [Component.Graph(), Component.Backlinks()],
+  left: [],
+  right: [Component.DesktopOnly(Component.TableOfContents()), Component.Backlinks()]
 }
+
+// export const defaultContentPageLayout: PageLayout = {
+//   beforeBody: [Component.ArticleTitle(), Component.ContentMeta(), Component.TagList()],
+//   left: [
+//     Component.PageTitle(),
+//     Component.MobileOnly(Component.Spacer()),
+//     Component.Search(),
+//     Component.Darkmode(),
+//     Component.DesktopOnly(Component.TableOfContents()),
+//   ],
+//   right: [Component.Graph(), Component.Backlinks()],
+// }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
+  header: [Component.PageTitle(), Component.Search(), Component.Darkmode()],
   beforeBody: [Component.ArticleTitle()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
-  ],
+  left: [],
   right: [],
 }
+
+// export const defaultListPageLayout: PageLayout = {
+//   beforeBody: [Component.ArticleTitle()],
+//   left: [
+//     Component.PageTitle(),
+//     Component.MobileOnly(Component.Spacer()),
+//     Component.Search(),
+//     Component.Darkmode(),
+//   ],
+//   right: [],
+// }
